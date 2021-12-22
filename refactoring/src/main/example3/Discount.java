ffb65f265ba771892dcfad6f83ca4c77da15b57b
MoreiraP12 <up201905429@edu.fe.up.pt>
2021-12-22 00:54:13 +0000

    Final commit

---
+++ refactoring/src/main/example3/Discount.java
@@ -0,0 +1,15 @@
+
+
+public abstract class Discount {
+    private final double discount;
+
+    public Discount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public abstract double applyDiscount(double price);
+}
---
+++ refactoring/src/main/example3/PercentageDiscount.java
@@ -0,0 +1,12 @@
+
+
+public class PercentageDiscount extends Discount {
+    public PercentageDiscount(double discount) {
+        super(discount);
+    }
+
+    @Override
+    public double applyDiscount(double price) {
+        return price - price * getDiscount();
+    }
+}
